1a1583dd391a336e6a65369331b5763767ff4a90
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-03-15 20:23:16 +0000

    recup du code de reflrctor

--- branches/recuperation/GestAdh45.Dao/SexeDao.cs
+++ branches/recuperation/GestAdh45.Dao/SexeDao.cs
@@ -1,50 +1,50 @@
-namespace GestAdh45.Dao
+﻿namespace GestAdh45.Dao
 {
-	public class SexeDao
-	{
-		private static SexeDao Instance;
-		private Entities Context
-		{
-			get;
-			set;
-		}
-		private SexeDao()
-		{
-		}
-		public static SexeDao GetInstance(Entities pContexte)
-		{
-			if (SexeDao.Instance == null)
-			{
-				SexeDao.Instance = new SexeDao();
-			}
-			SexeDao.Instance.Context = pContexte;
-			return SexeDao.Instance;
-		}
-		public Sexe Read(int pSexeId)
-		{
-			SexeDao.<>c__DisplayClass0 <>c__DisplayClass0 = new SexeDao.<>c__DisplayClass0();
-			<>c__DisplayClass0.pSexeId = pSexeId;
-			IQueryable<Sexe> arg_80_0 = SexeDao.Instance.Context.Sexes;
-			ParameterExpression parameterExpression = Expression.Parameter(typeof(Sexe), "s");
-			IQueryable<Sexe> source = Queryable.Where<Sexe>(arg_80_0, Expression.Lambda<Func<Sexe, bool>>(Expression.Equal(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_ID()))), Expression.Convert(Expression.Field(Expression.Constant(<>c__DisplayClass0), FieldInfo.GetFieldFromHandle(ldtoken(pSexeId))), typeof(long))), new ParameterExpression[]
-			{
-				parameterExpression
-			}));
-			return Queryable.First<Sexe>(source);
-		}
-		public List<Sexe> List()
-		{
-			IQueryable<Sexe> arg_4A_0 = SexeDao.Instance.Context.Sexes;
-			ParameterExpression parameterExpression = Expression.Parameter(typeof(Sexe), "s");
-			IOrderedQueryable<Sexe> source = Queryable.OrderBy<Sexe, string>(arg_4A_0, Expression.Lambda<Func<Sexe, string>>(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_LibelleCourt()))), new ParameterExpression[]
-			{
-				parameterExpression
-			}));
-			return Enumerable.ToList<Sexe>(source);
-		}
-		public void Refresh(Sexe pSexe)
-		{
-			this.Context.Refresh(RefreshMode.StoreWins, pSexe);
-		}
-	}
+    using GestAdh45.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Objects;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    public class SexeDao
+    {
+        private static SexeDao Instance;
+
+        private SexeDao()
+        {
+        }
+
+        public static SexeDao GetInstance(Entities pContexte)
+        {
+            if (Instance == null)
+            {
+                Instance = new SexeDao();
+            }
+            Instance.Context = pContexte;
+            return Instance;
+        }
+
+        public List<Sexe> List()
+        {
+            return (from s in Instance.Context.Sexes
+                orderby s.LibelleCourt
+                select s).ToList<Sexe>();
+        }
+
+        public Sexe Read(int pSexeId)
+        {
+            return (from s in Instance.Context.Sexes
+                where s.ID == pSexeId
+                select s).First<Sexe>();
+        }
+
+        public void Refresh(Sexe pSexe)
+        {
+            this.Context.Refresh(RefreshMode.StoreWins, pSexe);
+        }
+
+        private Entities Context { get; set; }
+    }
 }
+
--- branches/recuperation/GestAdh45.Dao/VilleDao.cs
+++ branches/recuperation/GestAdh45.Dao/VilleDao.cs
@@ -1,104 +1,91 @@
-namespace GestAdh45.Dao
+﻿namespace GestAdh45.Dao
 {
-	public class VilleDao
-	{
-		private static VilleDao Instance;
-		private Entities Context
-		{
-			get;
-			set;
-		}
-		private VilleDao()
-		{
-		}
-		public static VilleDao GetInstance(Entities pContexte)
-		{
-			if (VilleDao.Instance == null)
-			{
-				VilleDao.Instance = new VilleDao();
-			}
-			VilleDao.Instance.Context = pContexte;
-			return VilleDao.Instance;
-		}
-		public void Create(Ville pVille)
-		{
-			VilleDao.Instance.Context.Villes.AddObject(pVille);
-			VilleDao.Instance.Context.SaveChanges();
-		}
-		public Ville Read(int pVilleId)
-		{
-			VilleDao.<>c__DisplayClass0 <>c__DisplayClass0 = new VilleDao.<>c__DisplayClass0();
-			<>c__DisplayClass0.pVilleId = pVilleId;
-			IQueryable<Ville> arg_80_0 = VilleDao.Instance.Context.Villes;
-			ParameterExpression parameterExpression = Expression.Parameter(typeof(Ville), "v");
-			IQueryable<Ville> source = Queryable.Where<Ville>(arg_80_0, Expression.Lambda<Func<Ville, bool>>(Expression.Equal(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_ID()))), Expression.Convert(Expression.Field(Expression.Constant(<>c__DisplayClass0), FieldInfo.GetFieldFromHandle(ldtoken(pVilleId))), typeof(long))), new ParameterExpression[]
-			{
-				parameterExpression
-			}));
-			return Queryable.First<Ville>(source);
-		}
-		public void Update(Ville pVille)
-		{
-			EFExtension.SetAllModified<Ville>(pVille, VilleDao.Instance.Context);
-			try
-			{
-				VilleDao.Instance.Context.SaveChanges();
-			}
-			catch (OptimisticConcurrencyException arg_22_0)
-			{
-				OptimisticConcurrencyException optimisticConcurrencyException = arg_22_0;
-				throw optimisticConcurrencyException;
-			}
-		}
-		public void Delete(Ville pVille)
-		{
-			VilleDao.Instance.Context.Attach(pVille);
-			VilleDao.Instance.Context.DeleteObject(pVille);
-			VilleDao.Instance.Context.SaveChanges();
-		}
-		public List<Ville> List()
-		{
-			IQueryable<Ville> arg_4A_0 = VilleDao.Instance.Context.Villes;
-			ParameterExpression parameterExpression = Expression.Parameter(typeof(Ville), "v");
-			IOrderedQueryable<Ville> source = Queryable.OrderBy<Ville, string>(arg_4A_0, Expression.Lambda<Func<Ville, string>>(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_Libelle()))), new ParameterExpression[]
-			{
-				parameterExpression
-			}));
-			return Enumerable.ToList<Ville>(source);
-		}
-		public bool Exist(Ville pVille)
-		{
-			VilleDao.<>c__DisplayClass2 <>c__DisplayClass2 = new VilleDao.<>c__DisplayClass2();
-			<>c__DisplayClass2.pVille = pVille;
-			IQueryable<Ville> arg_107_0 = VilleDao.Instance.Context.Villes;
-			ParameterExpression parameterExpression = Expression.Parameter(typeof(Ville), "v");
-			IQueryable<Ville> source = Queryable.Where<Ville>(arg_107_0, Expression.Lambda<Func<Ville, bool>>(Expression.AndAlso(Expression.Call(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_CodePostal()))), (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(Equals())), new Expression[]
-			{
-				Expression.Property(Expression.Field(Expression.Constant(<>c__DisplayClass2), FieldInfo.GetFieldFromHandle(ldtoken(pVille))), (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_CodePostal())))
-			}), Expression.Call(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_Libelle()))), (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(Equals())), new Expression[]
-			{
-				Expression.Property(Expression.Field(Expression.Constant(<>c__DisplayClass2), FieldInfo.GetFieldFromHandle(ldtoken(pVille))), (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_Libelle())))
-			})), new ParameterExpression[]
-			{
-				parameterExpression
-			}));
-			return Queryable.Count<Ville>(source) > 0;
-		}
-		public bool IsUsed(Ville pVille)
-		{
-			VilleDao.<>c__DisplayClass4 <>c__DisplayClass4 = new VilleDao.<>c__DisplayClass4();
-			<>c__DisplayClass4.pVille = pVille;
-			IQueryable<Adresse> arg_85_0 = VilleDao.Instance.Context.Adresses;
-			ParameterExpression parameterExpression = Expression.Parameter(typeof(Adresse), "a");
-			IQueryable<Adresse> source = Queryable.Where<Adresse>(arg_85_0, Expression.Lambda<Func<Adresse, bool>>(Expression.Equal(Expression.Property(parameterExpression, (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_ID_Ville()))), Expression.Property(Expression.Field(Expression.Constant(<>c__DisplayClass4), FieldInfo.GetFieldFromHandle(ldtoken(pVille))), (MethodInfo)MethodBase.GetMethodFromHandle(ldtoken(get_ID())))), new ParameterExpression[]
-			{
-				parameterExpression
-			}));
-			return Queryable.Count<Adresse>(source) > 0;
-		}
-		public void Refresh(Ville pVille)
-		{
-			this.Context.Refresh(RefreshMode.StoreWins, pVille);
-		}
-	}
+    using GestAdh45.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Objects;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    public class VilleDao
+    {
+        private static VilleDao Instance;
+
+        private VilleDao()
+        {
+        }
+
+        public void Create(Ville pVille)
+        {
+            Instance.Context.Villes.AddObject(pVille);
+            Instance.Context.SaveChanges();
+        }
+
+        public void Delete(Ville pVille)
+        {
+            Instance.Context.Attach(pVille);
+            Instance.Context.DeleteObject(pVille);
+            Instance.Context.SaveChanges();
+        }
+
+        public bool Exist(Ville pVille)
+        {
+            return ((from v in Instance.Context.Villes
+                where v.CodePostal.Equals(pVille.CodePostal) && v.Libelle.Equals(pVille.Libelle)
+                select v).Count<Ville>() > 0);
+        }
+
+        public static VilleDao GetInstance(Entities pContexte)
+        {
+            if (Instance == null)
+            {
+                Instance = new VilleDao();
+            }
+            Instance.Context = pContexte;
+            return Instance;
+        }
+
+        public bool IsUsed(Ville pVille)
+        {
+            return ((from a in Instance.Context.Adresses
+                where a.ID_Ville == pVille.ID
+                select a).Count<Adresse>() > 0);
+        }
+
+        public List<Ville> List()
+        {
+            return (from v in Instance.Context.Villes
+                orderby v.Libelle
+                select v).ToList<Ville>();
+        }
+
+        public Ville Read(int pVilleId)
+        {
+            return (from v in Instance.Context.Villes
+                where v.ID == pVilleId
+                select v).First<Ville>();
+        }
+
+        public void Refresh(Ville pVille)
+        {
+            this.Context.Refresh(RefreshMode.StoreWins, pVille);
+        }
+
+        public void Update(Ville pVille)
+        {
+            pVille.SetAllModified<Ville>(Instance.Context);
+            try
+            {
+                Instance.Context.SaveChanges();
+            }
+            catch (OptimisticConcurrencyException exception)
+            {
+                throw exception;
+            }
+        }
+
+        private Entities Context { get; set; }
+    }
 }
+
