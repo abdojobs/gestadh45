ee5cce8908771a46a736f5cb464b972661cd84c4
guillaume.porte <guillaume.porte@gmail.com>
2011-03-15 23:38:38 +0000

    Ajout du projet gestadh45.Model

---
+++ gestadh45.Model/Ville.cs
@@ -0,0 +1,10 @@
+﻿
+namespace gestadh45.Model
+{
+	public partial class Ville
+	{
+		public override string ToString() {
+			return string.Format("{0} - {1}", this.CodePostal, this.Libelle);
+		}
+	}
+}
