0d756f82d057542a9b233c69e52f46cd6357356b
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-04-14 21:25:24 +0000

    travail en cours sur la création d'une nouvelle base

---
+++ trunk/gestadh45.service/Database/GenerateurDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SQLite;
+using System;
+
+namespace gestadh45.service.Database
+{
+	public class GenerateurDatabase
+	{
+		public string ConnectionString { get; set; }
+
+		public GenerateurDatabase(string pFilePath) {
+			if (!string.IsNullOrWhiteSpace(pFilePath)) {
+				SQLiteConnectionStringBuilder lBuilder = new SQLiteConnectionStringBuilder();
+				lBuilder.DataSource = pFilePath;
+				this.ConnectionString = lBuilder.ToString();
+			}
+		}
+
+		public void CreateDatabase() {
+			if (!string.IsNullOrWhiteSpace(this.ConnectionString)) {
+				using (SQLiteConnection lConnection = new SQLiteConnection(this.ConnectionString)) {
+					lConnection.Open();
+					using (SQLiteCommand lCommand = new SQLiteCommand(ResDatabase.SQL_Initialisation, lConnection)) {
+						lCommand.ExecuteNonQuery();
+						lConnection.Close();
+					}
+				}
+			}
+		}
+	}
+}
