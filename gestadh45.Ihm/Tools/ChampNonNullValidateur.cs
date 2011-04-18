f2059a8189fd081d1b72e2fa24e1d8d99182e468
guillaume.porte <guillaume.porte@gmail.com>
2011-04-18 22:07:33 +0000

    Validation sur les textbox

---
+++ gestadh45.Ihm/Tools/ChampNonNullValidateur.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using gestadh45.Ihm.ViewModel.Formulaire;
+
+namespace gestadh45.Ihm.Tools
+{
+	public class ChampNonNullValidateur : ValidationRule
+	{
+		public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo) {
+			if (value == null) {
+				return new ValidationResult(false, ResErreurs.ChampObligatoire);
+			}
+			else {
+				return new ValidationResult(true, null);
+			}
+		}
+	}
+}
