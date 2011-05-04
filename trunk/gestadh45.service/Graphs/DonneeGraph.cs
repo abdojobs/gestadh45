47e266ce2c3f8674d45fd2589a18be2ca7f38b34
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-05-04 22:37:30 +0000

    Refonte de la consultation des graphs pour utiliser le composant du WPF Toolkit au lieu de celui de Visifire

--- trunk/gestadh45.service/Graphs/DonneeGraph.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace gestadh45.service.Graphs
-{
-	public class DonneeGraph
-	{
-		public string Label { get; set; }
-		public double YValue { get; set; }
-	}
-}
--- trunk/gestadh45.service/Graphs/Graphique.cs
+++ trunk/gestadh45.service/Graphs/Graphique.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.ObjectModel;
-using Visifire.Charts;
+﻿using System.Collections.Generic;
 
 namespace gestadh45.service.Graphs
 {
 	public class Graphique
 	{
-		public ObservableCollection<DonneeGraph> Donnees { get; set; }
 		public string Titre { get; set; }
-		public RenderAs Type { get; set; }
+		public IDictionary<string, long> Donnees { get; set; }
+		public string NomDonnees { get; set; }
 	}
 }
