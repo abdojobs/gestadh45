24debad1cc697075744cabbaa40b7aaa3ab0248c
guillaume.porte <guillaume.porte@gmail.com>
2011-07-23 12:44:59 +0000

    début dao

---
+++ gestadh45.dao/IDao.cs
@@ -0,0 +1,15 @@
+﻿
+namespace gestadh45.dao1
+{
+	public interface IDao<T> : IReadOnlyDao<T>
+	{
+		T Create(T pDonnee);		
+		T Update(T pDonnee);
+		void Delete(T pDonnee);
+				
+		bool Exists(T pDonnee);
+		bool IsUsed(T pDonnee);
+
+		void Refresh(T pDonnee);
+	}
+}
