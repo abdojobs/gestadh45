3ae891ade488016ce853406d7a4a679e9d43cb96
guillaume.porte <guillaume.porte@gmail.com>
2011-07-23 12:47:27 +0000

    travail

---
+++ gestadh45.dao/SexeDao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using gestadh45.model;
+
+namespace gestadh45.dao
+{
+	public class SexeDao : IDao<Sexe>
+	{
+		public Sexe Create(Sexe pDonnee) {
+			throw new NotImplementedException();
+		}
+
+		public Sexe Update(Sexe pDonnee) {
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Sexe pDonnee) {
+			throw new NotImplementedException();
+		}
+
+		public bool Exists(Sexe pDonnee) {
+			throw new NotImplementedException();
+		}
+
+		public bool IsUsed(Sexe pDonnee) {
+			throw new NotImplementedException();
+		}
+
+		public void Refresh(Sexe pDonnee) {
+			throw new NotImplementedException();
+		}
+
+		public Sexe Read(int pId) {
+			throw new NotImplementedException();
+		}
+
+		public List<Sexe> List() {
+			throw new NotImplementedException();
+		}
+	}
+}
