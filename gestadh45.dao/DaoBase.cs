73e8f118821c06c835ffb74dff02a456454b7fbe
guillaume.porte <guillaume.porte@gmail.com>
2011-07-23 13:46:35 +0000

    travail

---
+++ gestadh45.dao/DaoBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SQLite;
+
+namespace gestadh45.dao
+{
+	public abstract class DaoBase
+	{
+		/// <summary>
+		/// Obtient/Définit la connexion à la base
+		/// </summary>
+		public SQLiteConnection Connection { get; set; }
+
+		/// <summary>
+		/// Constructeur
+		/// </summary>
+		/// <param name="pFilePath">Chemin du fichier base</param>
+		public DaoBase(string pFilePath) {
+			var scb = new SQLiteConnectionStringBuilder();
+			scb.DataSource = pFilePath;
+
+			this.Connection = new SQLiteConnection(scb.ToString());
+		}
+	}
+}
