56dc7e1e9a81f6d774781ac136d7c0dc984f03bf
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-07-24 10:21:36 +0000

    Ajout des dao ville et saison

---
+++ branches/new_dao/gestadh45.dao/SaisonDao.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using gestadh45.model;
+
+namespace gestadh45.dao
+{
+	public class SaisonDao : DaoBase, IDao<Saison>
+	{
+		public SaisonDao(string pFilePath) : base(pFilePath) { }
+
+		public int Create(Saison pDonnee) {
+			this.Connection.Open();
+
+			var paramEstSaisonCourante = new SQLiteParameter("@EstSaisonCourante", System.Data.DbType.Boolean) { Value = pDonnee.EstSaisonCourante };
+			var paramAnneeDebut = new SQLiteParameter("@AnneeDebut", System.Data.DbType.Int32) { Value = pDonnee.AnneeDebut };
+			var paramAnneeFin = new SQLiteParameter("@AnneeFin", System.Data.DbType.Int32) { Value = pDonnee.AnneeFin };
+
+			var cmd = new SQLiteCommand("INSERT INTO Saison(EstSaisonCourante, AnneeDebut, AnneeFin) Values(@EstSaisonCourante, @AnneeDebut, @AnneeFin);", this.Connection);
+			cmd.Parameters.Add(paramEstSaisonCourante);
+			cmd.Parameters.Add(paramAnneeDebut);
+			cmd.Parameters.Add(paramAnneeFin);
+
+			try {
+				cmd.ExecuteNonQuery();
+				return this.GetLastInsertId();
+			}
+			catch (SQLiteException) {
+				throw;
+			}
+			finally {
+				this.Connection.Close();
+			}
+		}
+
+		public void Update(Saison pDonnee) {
+			this.Connection.Open();
+
+			var paramId = new SQLiteParameter("@Id", System.Data.DbType.Int32) { Value = pDonnee.Id };
+			var paramEstSaisonCourante = new SQLiteParameter("@EstSaisonCourante", System.Data.DbType.Boolean) { Value = pDonnee.EstSaisonCourante };
+			var paramAnneeDebut = new SQLiteParameter("@AnneeDebut", System.Data.DbType.Int32) { Value = pDonnee.AnneeDebut };
+			var paramAnneeFin = new SQLiteParameter("@AnneeFin", System.Data.DbType.Int32) { Value = pDonnee.AnneeFin };
+
+			var cmd = new SQLiteCommand("UPDATE Saison SET EstSaisonCourante=@EstSaisonCourante, AnneeDebut=@AnneeDebut, AnneeFin=@AnneeFin WHERE ID=@Id;", this.Connection);
+			cmd.Parameters.Add(paramId);
+			cmd.Parameters.Add(paramEstSaisonCourante);
+			cmd.Parameters.Add(paramAnneeDebut);
+			cmd.Parameters.Add(paramAnneeFin);
+
+			try {
+				cmd.ExecuteNonQuery();
+			}
+			catch (SQLiteException) {
+				throw;
+			}
+			finally {
+				this.Connection.Close();
+			}
+		}
+
+		public void Delete(Saison pDonnee) {
+			this.Connection.Open();
+
+			var paramId = new SQLiteParameter("@Id", System.Data.DbType.Int32) { Value = pDonnee.Id };
+
+			var cmd = new SQLiteCommand("DELETE FROM Saison WHERE ID=@Id;", this.Connection);
+			cmd.Parameters.Add(paramId);
+
+			try {
+				cmd.ExecuteNonQuery();
+			}
+			catch (SQLiteException) {
+				throw;
+			}
+			finally {
+				this.Connection.Close();
+			}
+		}
+
+		public bool Exists(Saison pDonnee) {
+			this.Connection.Open();
+
+			var paramId = new SQLiteParameter("@Id", System.Data.DbType.Int32) { Value = pDonnee.Id };
+			var cmd = new SQLiteCommand("SELECT COUNT(*) FROM Saison WHERE ID=@Id;", this.Connection);
+			cmd.Parameters.Add(paramId);
+
+			try {
+				var result = (int)cmd.ExecuteScalar();
+				return result > 0;
+			}
+			catch (SQLiteException) {
+				throw;
+			}
+			finally {
+				this.Connection.Close();
+			}
+		}
+
+		public bool IsUsed(Saison pDonnee) {
+			this.Connection.Open();
+
+			var paramId = new SQLiteParameter("@Id", System.Data.DbType.Int32) { Value = pDonnee.Id };
+			var cmd = new SQLiteCommand("SELECT COUNT(*) FROM Groupe WHERE ID_Saison=@Id;", this.Connection);
+			cmd.Parameters.Add(paramId);
+
+			try {
+				var result = (int)cmd.ExecuteScalar();
+				return result > 0;
+			}
+			catch (SQLiteException) {
+				throw;
+			}
+			finally {
+				this.Connection.Close();
+			}
+		}
+
+		public Saison Read(int pId) {
+			this.Connection.Open();
+			Saison result = null;
+
+			var param = new SQLiteParameter("@Id", System.Data.DbType.Int32) { Value = pId };
+			var cmd = new SQLiteCommand("SELECT Id, EstSaisonCourante, AnneeDebut, AnneeFin FROM Saison WHERE ID=@Id;", this.Connection);
+			cmd.Parameters.Add(param);
+
+			var reader = cmd.ExecuteReader();
+
+			if (reader.HasRows) {
+				reader.Read();
+
+				result = new Saison()
+				{
+					Id = reader.GetInt32(0),
+					EstSaisonCourante = reader.GetBoolean(1),
+					AnneeDebut = reader.GetInt32(2),
+					AnneeFin = reader.GetInt32(3)
+				};
+			}
+
+			this.Connection.Close();
+			return result;
+		}
+
+		public List<Saison> List() {
+			this.Connection.Open();
+
+			List<Saison> result = new List<Saison>();
+
+			var cmd = new SQLiteCommand("SELECT Id, EstSaisonCourante, AnneeDebut, AnneeFin FROM Saison ORDER BY AnneeDebut;", this.Connection);
+
+			var reader = cmd.ExecuteReader();
+
+			if (reader.HasRows) {
+				while (reader.Read()) {
+					var donnee = new Saison()
+					{
+						Id = reader.GetInt32(0),
+						EstSaisonCourante = reader.GetBoolean(1),
+						AnneeDebut = reader.GetInt32(2),
+						AnneeFin = reader.GetInt32(3)
+					};
+
+					result.Add(donnee);
+				}
+			}
+
+			this.Connection.Close();
+			return result;
+		}
+	}
+}
--- branches/new_dao/gestadh45.tests/dao/RunTestsDao.cs
+++ branches/new_dao/gestadh45.tests/dao/RunTestsDao.cs
@@ -47,6 +47,40 @@ namespace gestadh45.tests.dao
 			Console.WriteLine("####################################################################################################");
 
 			Console.WriteLine();
+
+			Console.WriteLine("####################################################################################################");
+			Console.WriteLine(string.Format("Test de {0}...", typeof(InfosClubDao).ToString()));
+			Console.WriteLine();
+			TestInfosClubDao.InfosConnection(this._filePath);
+			TestInfosClubDao.TestRead(this._filePath);
+			Console.WriteLine(string.Format("Fin du test de {0}...", typeof(InfosClubDao).ToString()));
+			Console.WriteLine("####################################################################################################");
+
+			Console.WriteLine();
+
+			Console.WriteLine("####################################################################################################");
+			Console.WriteLine(string.Format("Test de {0}...", typeof(SaisonDao).ToString()));
+			Console.WriteLine();
+			TestSaisonDao.InfosConnection(this._filePath);
+			TestSaisonDao.TestRead(this._filePath, 0);
+			TestSaisonDao.TestRead(this._filePath, 4);
+			TestSaisonDao.TestList(this._filePath);
+			Console.WriteLine(string.Format("Fin du test de {0}...", typeof(SaisonDao).ToString()));
+			Console.WriteLine("####################################################################################################");
+
+			Console.WriteLine();
+
+			Console.WriteLine("####################################################################################################");
+			Console.WriteLine(string.Format("Test de {0}...", typeof(VilleDao).ToString()));
+			Console.WriteLine();
+			TestVilleDao.InfosConnection(this._filePath);
+			TestVilleDao.TestRead(this._filePath, 0);
+			TestVilleDao.TestRead(this._filePath, 28);
+			TestVilleDao.TestList(this._filePath);
+			Console.WriteLine(string.Format("Fin du test de {0}...", typeof(VilleDao).ToString()));
+			Console.WriteLine("####################################################################################################");
+
+			Console.WriteLine();
 		}
 	}
 }
