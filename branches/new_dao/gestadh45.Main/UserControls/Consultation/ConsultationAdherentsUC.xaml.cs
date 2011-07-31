1b3bc778bb06c1a44d91d87d029ecad9f92d2499
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-07-31 10:08:41 +0000

    travail en cours sur l'effacement du champ filtre ajout d'un startupsplashscreen

--- branches/new_dao/gestadh45.Main/UserControls/Consultation/ConsultationAdherentsUC.xaml.cs
+++ branches/new_dao/gestadh45.Main/UserControls/Consultation/ConsultationAdherentsUC.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace gestadh45.Main.UserControls.Consultation
 {
@@ -21,6 +10,12 @@ namespace gestadh45.Main.UserControls.Consultation
 	{
 		public ConsultationAdherentsUC() {
 			InitializeComponent();
+
+			Messenger.Default.Register<NotificationMessage>(this, (msg) => this.EffacerFiltre());
+		}
+
+		private void EffacerFiltre() {
+			this.tbxFiltre.Text = string.Empty;
 		}
 	}
 }
