759d3c3954d71de2a9c090a5105e16bf359499b6
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-08-19 17:00:49 +0000

    Mise en place de la dernière version du provider sqlite Ajout d'un service d'envoi de mail (non disponible pour le moment)

---
+++ branches/new_dao/gestadh45.service/Mail/MailHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mail;
+
+namespace gestadh45.service.Mail
+{
+	public class MailHelper
+	{
+		private DonneesMail _donnees;
+
+		public MailHelper(DonneesMail pDonnees) {
+			this._donnees = pDonnees;
+		}
+
+		public void Envoyer() {
+			SmtpClient serveur = new SmtpClient(this._donnees.ServeurSmtp);
+
+			MailMessage msg = new MailMessage()
+			{
+				Sender = new MailAddress(this._donnees.AdresseExpediteur, this._donnees.NomExpediteur),
+				From = new MailAddress(this._donnees.AdresseExpediteur, this._donnees.NomExpediteur),
+				Subject = this._donnees.Sujet,
+				Body = this._donnees.Message
+			};
+
+			foreach (string adr in this._donnees.DestinatairesPubliques) {
+				msg.CC.Add(adr);
+			}
+
+			foreach (string adr in this._donnees.DestinatairesPrives) {
+				msg.Bcc.Add(adr);
+			}
+
+			serveur.Send(msg);
+		}
+	}
+}
