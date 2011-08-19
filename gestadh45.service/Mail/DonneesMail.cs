302be1026922d0252e7a38c80d8b12d657bc5b47
guillaume.porte <guillaume.porte@gmail.com>
2011-08-19 17:00:49 +0000

    Mise en place de la dernière version du provider sqlite Ajout d'un service d'envoi de mail (non disponible pour le moment)

---
+++ gestadh45.service/Mail/DonneesMail.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace gestadh45.service.Mail
+{
+	public class DonneesMail
+	{
+		/// <summary>
+		/// Gets or sets the serveur SMTP.
+		/// </summary>
+		/// <value>
+		/// The serveur.
+		/// </value>
+		public string ServeurSmtp { get; set; }
+
+		/// <summary>
+		/// Gets or sets the adresse expediteur.
+		/// </summary>
+		/// <value>
+		/// The adresse expediteur.
+		/// </value>
+		public string AdresseExpediteur { get; set; }
+
+		/// <summary>
+		/// Gets or sets the nom expediteur.
+		/// </summary>
+		/// <value>
+		/// The nom expediteur.
+		/// </value>
+		public string NomExpediteur { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the destinataires publiques.
+		/// </summary>
+		/// <value>
+		/// The destinataires publiques.
+		/// </value>
+		public IEnumerable<string> DestinatairesPubliques { get; set; }
+
+		/// <summary>
+		/// Gets or sets the destinataires prives.
+		/// </summary>
+		/// <value>
+		/// The destinataires prives.
+		/// </value>
+		public IEnumerable<string> DestinatairesPrives { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the sujet.
+		/// </summary>
+		/// <value>
+		/// The sujet.
+		/// </value>
+		public string Sujet { get; set; }
+
+		/// <summary>
+		/// Gets or sets the message.
+		/// </summary>
+		/// <value>
+		/// The message.
+		/// </value>
+		public string Message { get; set; }
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DonneesMail"/> class.
+		/// </summary>
+		public DonneesMail() {
+			this.DestinatairesPubliques = new List<string>();
+			this.DestinatairesPrives = new List<string>();
+		}
+	}
+}
