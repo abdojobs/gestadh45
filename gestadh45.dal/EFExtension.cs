0645db77e684266a25effe54671a443fe3c74069
guillaume.porte <guillaume.porte@gmail.com>
2011-09-08 19:12:50 +0000

    Ajout du projet dal avec le nouveau edmx

---
+++ gestadh45.dal/EFExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+using System.Linq;
+
+namespace gestadh45.dal
+{
+	public static class EFExtension
+	{
+		public static void SetAllModified<T>(this T entity, ObjectContext context) where T : IEntityWithKey {
+			var stateEntry = context.ObjectStateManager.GetObjectStateEntry(entity.EntityKey);
+			var propertyNameList = stateEntry.CurrentValues.DataRecordInfo.FieldMetadata.Select
+			  (pn => pn.FieldType.Name);
+			foreach (var propName in propertyNameList)
+				stateEntry.SetModifiedProperty(propName);
+		}
+	}
+}
