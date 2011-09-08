c1f27a25173260c8bd3c2d8f125cb44e62f83d06
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-09-08 21:32:04 +0000

    Simplification vvm locator

--- trunk/gestadh45.Ihm/ViewModel/ViewModelLocator.cs
+++ trunk/gestadh45.Ihm/ViewModel/ViewModelLocator.cs
@@ -1,5 +1,4 @@
 ﻿using gestadh45.Ihm.ViewModel.Adherents;
-using gestadh45.Ihm.ViewModel.Common;
 using gestadh45.Ihm.ViewModel.Groupes;
 using gestadh45.Ihm.ViewModel.InfosClubs;
 using gestadh45.Ihm.ViewModel.Inscriptions;
@@ -11,342 +10,251 @@ namespace gestadh45.Ihm.ViewModel
 {
 	public class ViewModelLocator
 	{
-		private static MainViewModel _main;
-		private static ConsultationAdherentsUCViewModel mConsultationAdherentsVM;
-		private static ConsultationGroupesUCViewModel mConsultationGroupesVM;
-		private static ConsultationInfosClubUCViewModel mConsultationInfosClubVM;
-		private static ConsultationInscriptionsUCViewModel mConsultationInscriptionsVM;
-		private static ConsultationSaisonsUCViewModel mConsultationSaisonsVM;
-		private static ConsultationVillesUCViewModel mConsultationVillesVM;
-		private static ConsultationExtractionsUCViewModel mConsultationExtractionsVM;
-		private static FormulaireAdherentUCViewModel mFormulaireAdherentVM;
-		private static FormulaireGroupeUCViewModel mFormulaireGroupeVM;
-		private static FormulaireInfosClubUCViewModel mFormulaireInfosClubVM;
-		private static FormulaireInscriptionUCViewModel mFormulaireInscriptionVM;
-		private static FormulaireSaisonUCViewModel mFormulaireSaisonVM;
-		private static FormulaireVilleUCViewModel mFormulaireVilleVM;
-		private static GraphsSaisonCouranteUCViewModel mGraphsSaisonCouranteVM;
+		public static string DataSource = null;
 
 		public ViewModelLocator() {
-			CreateMain();
-		}
-
-		public static void Cleanup() {
-			ClearMain();
-		}
-
-		public static void ClearConsultationAdherentsVMStatic() {
-			if (mConsultationAdherentsVM != null) {
-				mConsultationAdherentsVM.Cleanup();
-				mConsultationAdherentsVM = null;
-			}
-		}
-
-		public static void ClearConsultationGroupesVMStatic() {
-			if (mConsultationGroupesVM != null) {
-				mConsultationGroupesVM.Cleanup();
-				mConsultationGroupesVM = null;
-			}
-		}
-
-		public static void ClearConsultationInfosClubVMStatic() {
-			if (mConsultationInfosClubVM != null) {
-				mConsultationInfosClubVM.Cleanup();
-				mConsultationInfosClubVM = null;
-			}
-		}
-
-		public static void ClearConsultationInscriptionsVMStatic() {
-			if (mConsultationInscriptionsVM != null) {
-				mConsultationInscriptionsVM.Cleanup();
-				mConsultationInscriptionsVM = null;
-			}
-		}
-
-		public static void ClearConsultationSaisonsVMStatic() {
-			if (mConsultationSaisonsVM != null) {
-				mConsultationSaisonsVM.Cleanup();
-				mConsultationSaisonsVM = null;
-			}
-		}
-
-		public static void ClearConsultationVillesVMStatic() {
-			if (mConsultationVillesVM != null) {
-				mConsultationVillesVM.Cleanup();
-				mConsultationVillesVM = null;
-			}
-		}
-
-		public static void ClearConsultationExtractionsVMStatic() {
-			if (mConsultationExtractionsVM != null) {
-				mConsultationExtractionsVM.Cleanup();
-				mConsultationExtractionsVM = null;
-			}
-		}
-
-		public static void ClearFormulaireAdherentVMStatic() {
-			if (mFormulaireAdherentVM != null) {
-				mFormulaireAdherentVM.Cleanup();
-				mFormulaireAdherentVM = null;
-			}
-		}
-
-		public static void ClearFormulaireGroupeVMStatic() {
-			if (mFormulaireGroupeVM != null) {
-				mFormulaireGroupeVM.Cleanup();
-				mFormulaireGroupeVM = null;
-			}
-		}
-
-		public static void ClearFormulaireInfosClubVMStatic() {
-			if (mFormulaireInfosClubVM != null) {
-				mFormulaireInfosClubVM.Cleanup();
-				mFormulaireInfosClubVM = null;
-			}
-		}
-
-		public static void ClearFormulaireInscriptionVMStatic() {
-			if (mFormulaireInscriptionVM != null) {
-				mFormulaireInscriptionVM.Cleanup();
-				mFormulaireInscriptionVM = null;
-			}
-		}
-
-		public static void ClearFormulaireSaisonVMStatic() {
-			if (mFormulaireSaisonVM != null) {
-				mFormulaireSaisonVM.Cleanup();
-				mFormulaireSaisonVM = null;
-			}
-		}
-
-		public static void ClearFormulaireVilleVMStatic() {
-			if (mFormulaireVilleVM != null) {
-				mFormulaireVilleVM.Cleanup();
-				mFormulaireVilleVM = null;
-			}
-		}
-
-		public static void ClearGraphsSaisonCouranteVMStatic() {
-			if (mGraphsSaisonCouranteVM != null) {
-				mGraphsSaisonCouranteVM.Cleanup();
-				mGraphsSaisonCouranteVM = null;
-			}
-		}
-
-		public static void ClearMain() {
-			_main.Cleanup();
-			_main = null;
-		}
-
-		public static void CreateConsultationAdherentsVMStatic() {
-			if (mConsultationAdherentsVM == null) {
-				mConsultationAdherentsVM = new ConsultationAdherentsUCViewModel();
-			}
-		}
-
-		public static void CreateConsultationGroupesVMStatic() {
-			if (mConsultationGroupesVM == null) {
-				mConsultationGroupesVM = new ConsultationGroupesUCViewModel();
-			}
-		}
-
-		public static void CreateConsultationInfosClubVMStatic() {
-			if (mConsultationInfosClubVM == null) {
-				mConsultationInfosClubVM = new ConsultationInfosClubUCViewModel();
-			}
-		}
-
-		public static void CreateConsultationInscriptionsVMStatic() {
-			if (mConsultationInscriptionsVM == null) {
-				mConsultationInscriptionsVM = new ConsultationInscriptionsUCViewModel();
-			}
-		}
-
-		public static void CreateConsultationSaisonsVMStatic() {
-			if (mConsultationSaisonsVM == null) {
-				mConsultationSaisonsVM = new ConsultationSaisonsUCViewModel();
-			}
-		}
-
-		public static void CreateConsultationVillesVMStatic() {
-			if (mConsultationVillesVM == null) {
-				mConsultationVillesVM = new ConsultationVillesUCViewModel();
-			}
-		}
-
-		public static void CreateConsultationExtractionsVMStatic() {
-			if (mConsultationExtractionsVM == null) {
-				mConsultationExtractionsVM = new ConsultationExtractionsUCViewModel();
-			}
-		}
-
-		public static void CreateFormulaireAdherentVMStatic() {
-			if (mFormulaireAdherentVM == null) {
-				mFormulaireAdherentVM = new FormulaireAdherentUCViewModel();
-			}
-		}
-
-		public static void CreateFormulaireGroupeVMStatic() {
-			if (mFormulaireGroupeVM == null) {
-				mFormulaireGroupeVM = new FormulaireGroupeUCViewModel();
-			}
-		}
-
-		public static void CreateFormulaireInfosClubVMStatic() {
-			if (mFormulaireInfosClubVM == null) {
-				mFormulaireInfosClubVM = new FormulaireInfosClubUCViewModel();
-			}
-		}
-
-		public static void CreateFormulaireInscriptionVMStatic() {
-			if (mFormulaireInscriptionVM == null) {
-				mFormulaireInscriptionVM = new FormulaireInscriptionUCViewModel();
-			}
-		}
-
-		public static void CreateFormulaireSaisonVMStatic() {
-			if (mFormulaireSaisonVM == null) {
-				mFormulaireSaisonVM = new FormulaireSaisonUCViewModel();
-			}
-		}
-
-		public static void CreateFormulaireVilleVMStatic() {
-			if (mFormulaireVilleVM == null) {
-				mFormulaireVilleVM = new FormulaireVilleUCViewModel();
-			}
-		}
-
-		public static void CreateGraphsSaisonCouranteVMStatic() {
-			if (mGraphsSaisonCouranteVM == null) {
-				mGraphsSaisonCouranteVM = new GraphsSaisonCouranteUCViewModel();
-			}
-		}
-
-		public static void CreateMain() {
 			if (_main == null) {
 				_main = new MainViewModel();
 			}
 		}
 
-		public static ConsultationAdherentsUCViewModel ConsultationAdherentsVMStatic {
-			get {
-				ClearConsultationAdherentsVMStatic();
-				CreateConsultationAdherentsVMStatic();
-				return mConsultationAdherentsVM;
-			}
+		public static void Cleanup() {
+			_main.Cleanup();
+			_main = null;
 		}
 
-		public static ConsultationGroupesUCViewModel ConsultationGroupesVMStatic {
-			get {
-				ClearConsultationGroupesVMStatic();
-				CreateConsultationGroupesVMStatic();
-				return mConsultationGroupesVM;
-			}
-		}
-
-		public static ConsultationInfosClubUCViewModel ConsultationInfosClubVMStatic {
-			get {
-				ClearConsultationInfosClubVMStatic();
-				CreateConsultationInfosClubVMStatic();
-				return mConsultationInfosClubVM;
-			}
-		}
-
-		public static ConsultationInscriptionsUCViewModel ConsultationInscriptionsVMStatic {
-			get {
-				ClearConsultationInscriptionsVMStatic();
-				CreateConsultationInscriptionsVMStatic();
-				return mConsultationInscriptionsVM;
-			}
-		}
-
-		public static ConsultationSaisonsUCViewModel ConsultationSaisonsVMStatic {
-			get {
-				ClearConsultationSaisonsVMStatic();
-				CreateConsultationSaisonsVMStatic();
-				return mConsultationSaisonsVM;
-			}
-		}
-
-		public static ConsultationVillesUCViewModel ConsultationVillesVMStatic {
-			get {
-				ClearConsultationVillesVMStatic();
-				CreateConsultationVillesVMStatic();
-				return mConsultationVillesVM;
-			}
-		}
-
-		public static ConsultationExtractionsUCViewModel ConsultationExtractionsVMStatic {
-			get {
-				ClearConsultationExtractionsVMStatic();
-				CreateConsultationExtractionsVMStatic();
-				return mConsultationExtractionsVM;
-			}
-		}
-
-		public static FormulaireAdherentUCViewModel FormulaireAdherentVMStatic {
-			get {
-				ClearFormulaireAdherentVMStatic();
-				CreateFormulaireAdherentVMStatic();
-				return mFormulaireAdherentVM;
-			}
-		}
-
-		public static FormulaireGroupeUCViewModel FormulaireGroupeVMStatic {
-			get {
-				ClearFormulaireGroupeVMStatic();
-				CreateFormulaireGroupeVMStatic();
-				return mFormulaireGroupeVM;
-			}
-		}
-
-		public static FormulaireInfosClubUCViewModel FormulaireInfosClubVMStatic {
-			get {
-				ClearFormulaireInfosClubVMStatic();
-				CreateFormulaireInfosClubVMStatic();
-				return mFormulaireInfosClubVM;
-			}
-		}
-
-		public static FormulaireInscriptionUCViewModel FormulaireInscriptionVMStatic {
-			get {
-				ClearFormulaireInscriptionVMStatic();
-				CreateFormulaireInscriptionVMStatic();
-				return mFormulaireInscriptionVM;
-			}
-		}
-
-		public static FormulaireSaisonUCViewModel FormulaireSaisonVMStatic {
-			get {
-				ClearFormulaireSaisonVMStatic();
-				CreateFormulaireSaisonVMStatic();
-				return mFormulaireSaisonVM;
-			}
-		}
-
-		public static FormulaireVilleUCViewModel FormulaireVilleVMStatic {
-			get {
-				ClearFormulaireVilleVMStatic();
-				CreateFormulaireVilleVMStatic();
-				return mFormulaireVilleVM;
-			}
-		}
-
-		public static GraphsSaisonCouranteUCViewModel GraphsSaisonCouranteVMStatic {
-			get {
-				ClearGraphsSaisonCouranteVMStatic();
-				CreateGraphsSaisonCouranteVMStatic();
-				return mGraphsSaisonCouranteVM;
-			}
-		}
+		#region MainView
+		private static MainViewModel _main;
 
 		public static MainViewModel MainStatic {
 			get {
 				if (_main == null) {
-					CreateMain();
+					_main = new MainViewModel();
 				}
 				return _main;
 			}
 		}
+		#endregion
+
+		#region ConsultationInfosClub
+		private static ConsultationInfosClubUCViewModel _consultationInfosClubVM;
+
+		public static ConsultationInfosClubUCViewModel ConsultationInfosClubVMStatic {
+			get {
+				if (_consultationInfosClubVM != null) {
+					_consultationInfosClubVM.Cleanup();
+					_consultationInfosClubVM = null;
+				}
+
+				_consultationInfosClubVM = new ConsultationInfosClubUCViewModel();
+
+				return _consultationInfosClubVM;
+			}
+		}
+		#endregion
+
+		#region FormulaireInfosClub
+		private static FormulaireInfosClubUCViewModel _formulaireInfosClubVM;
+
+		public static FormulaireInfosClubUCViewModel FormulaireInfosClubVMStatic {
+			get {
+				if (_formulaireInfosClubVM != null) {
+					_formulaireInfosClubVM.Cleanup();
+					_formulaireInfosClubVM = null;
+				}
+
+				_formulaireInfosClubVM = new FormulaireInfosClubUCViewModel();
+
+				return _formulaireInfosClubVM;
+			}
+		}
+		#endregion
+
+		#region ConsultationSaisons
+		private static ConsultationSaisonsUCViewModel _consultationSaisonsVM;
+
+		public static ConsultationSaisonsUCViewModel ConsultationSaisonsVMStatic {
+			get {
+				if (_consultationSaisonsVM != null) {
+					_consultationSaisonsVM.Cleanup();
+					_consultationSaisonsVM = null;
+				}
+
+				_consultationSaisonsVM = new ConsultationSaisonsUCViewModel();
+
+				return _consultationSaisonsVM;
+			}
+		}
+		#endregion
+
+		#region FormulaireSaison
+		private static FormulaireSaisonUCViewModel _formulaireSaisonVM;
+
+		public static FormulaireSaisonUCViewModel FormulaireSaisonVMStatic {
+			get {
+				if (_formulaireSaisonVM != null) {
+					_formulaireSaisonVM.Cleanup();
+					_formulaireSaisonVM = null;
+				}
+
+				_formulaireSaisonVM = new FormulaireSaisonUCViewModel();
+
+				return _formulaireSaisonVM;
+			}
+		}
+		#endregion
+
+		#region ConsultationVilles
+		private static ConsultationVillesUCViewModel _consultationVillesVM;
+
+		public static ConsultationVillesUCViewModel ConsultationVillesVMStatic {
+			get {
+				if (_consultationVillesVM != null) {
+					_consultationVillesVM.Cleanup();
+					_consultationVillesVM = null;
+				}
+
+				_consultationVillesVM = new ConsultationVillesUCViewModel();
+
+				return _consultationVillesVM;
+			}
+		}
+		#endregion
+
+		#region FormulaireVille
+		private static FormulaireVilleUCViewModel _formulaireVilleVM;
+
+		public static FormulaireVilleUCViewModel FormulaireVilleVMStatic {
+			get {
+				if (_formulaireVilleVM != null) {
+					_formulaireVilleVM.Cleanup();
+					_formulaireVilleVM = null;
+				}
+
+				_formulaireVilleVM = new FormulaireVilleUCViewModel();
+
+				return _formulaireVilleVM;
+			}
+		}
+		#endregion
+
+		#region ConsultationAdherents
+		private static ConsultationAdherentsUCViewModel _consultationAdherentsVM;
+
+		public static ConsultationAdherentsUCViewModel ConsultationAdherentsVMStatic {
+			get {
+				if (_consultationAdherentsVM != null) {
+					_consultationAdherentsVM.Cleanup();
+					_consultationAdherentsVM = null;
+				}
+
+				_consultationAdherentsVM = new ConsultationAdherentsUCViewModel();
+
+				return _consultationAdherentsVM;
+			}
+		}
+		#endregion
+
+		#region FormulaireAdherent
+		private static FormulaireAdherentUCViewModel _formulaireAdherentVM;
+
+		public static FormulaireAdherentUCViewModel FormulaireAdherentVMStatic {
+			get {
+				if (_formulaireAdherentVM != null) {
+					_formulaireAdherentVM.Cleanup();
+					_formulaireAdherentVM = null;
+				}
+
+				_formulaireAdherentVM = new FormulaireAdherentUCViewModel();
+
+				return _formulaireAdherentVM;
+			}
+		}
+		#endregion
+
+		#region ConsultationsInscriptions
+		private static ConsultationInscriptionsUCViewModel _consultationInscriptionsVM;
+
+		public static ConsultationInscriptionsUCViewModel ConsultationInscriptionsVMStatic {
+			get {
+				if (_consultationInscriptionsVM != null) {
+					_consultationInscriptionsVM.Cleanup();
+					_consultationInscriptionsVM = null;
+				}
+
+				_consultationInscriptionsVM = new ConsultationInscriptionsUCViewModel();
+
+				return _consultationInscriptionsVM;
+			}
+		}
+		#endregion
+
+		#region FormulaireInscription
+		private static FormulaireInscriptionUCViewModel _formulaireInscriptionVM;
+
+		public static FormulaireInscriptionUCViewModel FormulaireInscriptionVMStatic {
+			get {
+				if (_formulaireInscriptionVM != null) {
+					_formulaireInscriptionVM.Cleanup();
+					_formulaireInscriptionVM = null;
+				}
+
+				_formulaireInscriptionVM = new FormulaireInscriptionUCViewModel();
+
+				return _formulaireInscriptionVM;
+			}
+		}
+		#endregion
+
+		#region ConsultationGroupes
+		private static ConsultationGroupesUCViewModel _consultationGroupesVM;
+
+		public static ConsultationGroupesUCViewModel ConsultationGroupesVMStatic {
+			get {
+				if (_consultationGroupesVM != null) {
+					_consultationGroupesVM.Cleanup();
+					_consultationGroupesVM = null;
+				}
+
+				_consultationGroupesVM = new ConsultationGroupesUCViewModel();
+
+				return _consultationGroupesVM;
+			}
+		}
+		#endregion
+
+		#region FormulaireGroupe
+		private static FormulaireGroupeUCViewModel _formulaireGroupeVM;
+
+		public static FormulaireGroupeUCViewModel FormulaireGroupeVMStatic {
+			get {
+				if (_formulaireGroupeVM != null) {
+					_formulaireGroupeVM.Cleanup();
+					_formulaireGroupeVM = null;
+				}
+
+				_formulaireGroupeVM = new FormulaireGroupeUCViewModel();
+
+				return _formulaireGroupeVM;
+			}
+		}
+		#endregion
+
+		#region GraphsSaisonCourante
+		private static GraphsSaisonCouranteUCViewModel _graphsSaisonCouranteVM;
+
+		public static GraphsSaisonCouranteUCViewModel GraphsSaisonCouranteVMStatic {
+			get {
+				if (_graphsSaisonCouranteVM != null) {
+					_graphsSaisonCouranteVM.Cleanup();
+					_graphsSaisonCouranteVM = null;
+				}
+
+				_graphsSaisonCouranteVM = new GraphsSaisonCouranteUCViewModel();
+
+				return _graphsSaisonCouranteVM;
+			}
+		}
+		#endregion
 	}
 }
