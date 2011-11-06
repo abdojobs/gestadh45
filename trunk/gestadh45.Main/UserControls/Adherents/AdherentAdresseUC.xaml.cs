7cfd1a8f58797fab9f15c8d054cda95c96fe2dc5
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-11-06 11:19:03 +0000

    Modification de l'ércan de consultation adhérents pour utiliser des UC

---
+++ trunk/gestadh45.Main/UserControls/Adherents/AdherentAdresseUC.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using gestadh45.dal;
+
+namespace gestadh45.Main.UserControls.Adherents
+{
+	/// <summary>
+	/// Logique d'interaction pour AdherentAdresseUC.xaml
+	/// </summary>
+	public partial class AdherentAdresseUC : UserControl
+	{
+		#region proprietes
+		/// <summary>
+		/// Obtient/Définit l'adhérent
+		/// </summary>
+		public Adherent Adherent {
+			get {
+				return (Adherent)GetValue(AdherentProperty);
+			}
+
+			set {
+				SetValue(AdherentProperty, value);
+			}
+		}
+		#endregion
+		
+		public AdherentAdresseUC() {
+			InitializeComponent();
+		}
+
+		#region Dependency Properties
+		public static DependencyProperty AdherentProperty = DependencyProperty.Register(
+			"Adherent",
+			typeof(Adherent),
+			typeof(AdherentAdresseUC)
+		);
+		#endregion
+	}
+}
