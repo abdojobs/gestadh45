c19a4dec58408c9b531a7d0016fa0e8d812c81ea
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2011-11-06 11:19:03 +0000

    Modification de l'ércan de consultation adhérents pour utiliser des UC

---
+++ gestadh45.Main/UserControls/Adherents/AdherentContactUC.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using gestadh45.dal;
+
+namespace gestadh45.Main.UserControls.Adherents
+{
+	/// <summary>
+	/// Logique d'interaction pour AdherentContactUC.xaml
+	/// </summary>
+	public partial class AdherentContactUC : UserControl
+	{
+		#region proprietes
+		/// <summary>
+		/// Obtient/Définit l'adhérent
+		/// </summary>
+		public Adherent Adherent {
+			get {
+				return (Adherent)GetValue(AdherentProperty);
+			}
+
+			set {
+				SetValue(AdherentProperty, value);
+			}
+		}
+		#endregion
+
+		public AdherentContactUC() {
+			InitializeComponent();
+		}
+
+		#region Dependency Properties
+		public static DependencyProperty AdherentProperty = DependencyProperty.Register(
+			"Adherent",
+			typeof(Adherent),
+			typeof(AdherentContactUC)
+		);
+		#endregion
+	}
+}
---
+++ gestadh45.Main/UserControls/Adherents/AdherentIdentiteUC.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using gestadh45.dal;
+
+namespace gestadh45.Main.UserControls.Adherents
+{
+	/// <summary>
+	/// Logique d'interaction pour AdherentIdentiteUC.xaml
+	/// </summary>
+	public partial class AdherentIdentiteUC : UserControl
+	{
+		#region proprietes
+		/// <summary>
+		/// Obtient/Définit l'adhérent
+		/// </summary>
+		public Adherent Adherent {
+			get {
+				return (Adherent)GetValue(AdherentProperty);
+			}
+
+			set {
+				SetValue(AdherentProperty, value);
+			}
+		}
+		#endregion
+
+		public AdherentIdentiteUC() {
+			InitializeComponent();
+		}
+
+		#region Dependency Properties
+		public static DependencyProperty AdherentProperty = DependencyProperty.Register(
+			"Adherent",
+			typeof(Adherent),
+			typeof(AdherentIdentiteUC)
+		);
+		#endregion
+	}
+}
