ffe8e90c4def294661bc27ba0e62d18001bb4534
guillaume.porte <guillaume.porte@gmail.com>
2012-02-15 13:40:50 +0000

    création du projet dal

--- gestadh45.Ihm/SpecialMessages/MsgClose.cs
+++
@@ -1,9 +0,0 @@
-﻿using GalaSoft.MvvmLight.Messaging;
-
-namespace gestadh45.Ihm.SpecialMessages
-{
-	public class MsgClose : NotificationMessage
-	{
-		public MsgClose() : base(TypesNotification.NotificationClose) {}
-	}
-}
