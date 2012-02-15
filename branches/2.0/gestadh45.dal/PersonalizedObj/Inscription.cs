cfdbaaac422c2e60a25538bd3ba118d3667f431b
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-02-15 14:09:23 +0000

    Création du contexte + objets personnalisés

---
+++ branches/2.0/gestadh45.dal/PersonalizedObj/Inscription.cs
@@ -0,0 +1,14 @@
+﻿
+namespace gestadh45.dal
+{
+	public partial class Inscription
+	{
+		/// <summary>
+		/// Obtient la description de l'inscription
+		/// </summary>
+		/// <returns>Adherent - LibelleGroupe</returns>
+		public override string ToString() {
+			return string.Format("{0} - {1}", this.Adherent, this.Groupe.Libelle);
+		}
+	}
+}
---
+++ branches/2.0/gestadh45.dal/PersonalizedObj/Sexe.cs
@@ -0,0 +1,22 @@
+﻿
+namespace gestadh45.dal
+{
+	public partial class Sexe
+	{
+		/// <summary>
+		/// Obtient une description du sexe
+		/// </summary>
+		/// <returns>LibelleLong</returns>
+		public string ToLongString() {
+			return this.LibelleLong;
+		}
+
+		/// <summary>
+		/// Obtient une description courte du sexe
+		/// </summary>
+		/// <returns>LibelleCourt</returns>
+		public override string ToString() {
+			return this.LibelleCourt;
+		}
+	}
+}
