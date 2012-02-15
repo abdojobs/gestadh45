cab43f06f630243d7ebf0c493383f99c96def9bf
guillaume.porte <guillaume.porte@gmail.com>
2012-02-15 14:23:25 +0000

    Ajout et initialisation du projet Manager

---
+++ gestadh45.manager/ContextManager.cs
@@ -0,0 +1,28 @@
+﻿
+using gestadh45.dal;
+
+namespace gestadh45.manager
+{
+	public class ContextManager
+	{
+		/// <summary>
+		/// Obtient le contexte de l'application
+		/// </summary>
+		public GestAdh45Entities Context { get; internal set; }
+
+		/// <summary>
+		/// Initialise une nouvelle instance du contexte
+		/// </summary>
+		public ContextManager() {
+			this.Context = new GestAdh45Entities();
+		}
+
+		/// <summary>
+		/// Ferme et détruit l'instance du contexte
+		/// </summary>
+		public void CloseContext() {
+			this.Context.Dispose();
+			this.Context = null;
+		}
+	}
+}
