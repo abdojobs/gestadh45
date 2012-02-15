39846a6ae87780b55e822da70042a7ae6f9105cd
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-02-15 16:51:50 +0000

    ajout du projet console

---
+++ branches/2.0/gestadh45.consoletest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using gestadh45.dal;
+
+namespace gestadh45.consoletest
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			var repoAdh = new Repository<Adherent>();
+
+			Console.WriteLine(repoAdh.GetById(64));
+
+			Console.ReadKey();
+		}
+	}
+}
