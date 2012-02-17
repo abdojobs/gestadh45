012711245246cc22afa0e7c5d5583a09509fa860
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-02-17 17:04:11 +0000

    travail

---
+++ branches/2.0/gestadh45.wpf/UserControls/MenuPrincipalUC.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace gestadh45.wpf.UserControls
+{
+	/// <summary>
+	/// Logique d'interaction pour MenuPrincipalUC.xaml
+	/// </summary>
+	public partial class MenuPrincipalUC : UserControl
+	{
+		#region proprietes
+		/// <summary>
+		/// Obtient/Définit la commande d'affichage d'UC
+		/// </summary>
+		public ICommand CmdShowUC {
+			get {
+				return (ICommand)GetValue(CmdShowUCProperty);
+			}
+
+			set {
+				SetValue(CmdShowUCProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// Obtient/Définit la commande d'affichage de l'about box
+		/// </summary>
+		public ICommand CmdAboutBox {
+			get {
+				return (ICommand)GetValue(CmdAboutBoxProperty);
+			}
+
+			set {
+				SetValue(CmdAboutBoxProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// Obtient/Définit la commande de fermeture de l'application
+		/// </summary>
+		public ICommand CmdClose {
+			get {
+				return (ICommand)GetValue(CmdCloseProperty);
+			}
+
+			set {
+				SetValue(CmdCloseProperty, value);
+			}
+		}
+		#endregion
+
+		public MenuPrincipalUC() {
+			InitializeComponent();
+		}
+
+		#region Dependency Properties
+		public static DependencyProperty CmdShowUCProperty = DependencyProperty.Register(
+			"CmdShowUC",
+			typeof(ICommand),
+			typeof(MenuPrincipalUC)
+		);
+
+		public static DependencyProperty CmdAboutBoxProperty = DependencyProperty.Register(
+			"CmdAboutBox",
+			typeof(ICommand),
+			typeof(MenuPrincipalUC)
+		);
+
+		public static DependencyProperty CmdCloseProperty = DependencyProperty.Register(
+			"CmdClose",
+			typeof(ICommand),
+			typeof(MenuPrincipalUC)
+		);
+		#endregion
+	}
+}
