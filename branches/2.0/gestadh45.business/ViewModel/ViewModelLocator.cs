978f87920a9bc28b22767c61b16ad1ae0deaf7a3
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-02-17 13:03:40 +0000

    travail (projet wpf)

--- branches/2.0/gestadh45.business/ViewModel/ViewModelLocator.cs
+++ branches/2.0/gestadh45.business/ViewModel/ViewModelLocator.cs
@@ -1,58 +1,34 @@
 /*
   In App.xaml:
   <Application.Resources>
-      <vm:ViewModelLocatorTemplate xmlns:vm="clr-namespace:gestadh45.business"
-                                   x:Key="Locator" />
+	  <vm:ViewModelLocatorTemplate xmlns:vm="clr-namespace:gestadh45.business"
+								   x:Key="Locator" />
   </Application.Resources>
   
   In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
+  DataContext="{Binding Source={StaticResource Locator}, Path=[StaticVMProperty]}"
 */
 
 using GalaSoft.MvvmLight;
+using gestadh45.business.ViewModel.InfosClubVM;
 
 namespace gestadh45.business.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// </summary>
-    public class ViewModelLocator
-    {
-        private static MainViewModel _main;
+	public static class ViewModelLocator
+	{
+		public static MainViewModel Main
+		{
+			get { return new MainViewModel(); }
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the ViewModelLocator class.
-        /// </summary>
-        public ViewModelLocator()
-        {
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time services and viewmodels
-            ////}
-            ////else
-            ////{
-            ////    // Create run time services and view models
-            ////}
+		#region InfosClubVM
+		public static ConsultationInfosClubVM ConsultationInfosClubVM {
+			get { return new ConsultationInfosClubVM(); }
+		}
 
-            _main = new MainViewModel();
-        }
-
-        /// <summary>
-        /// Gets the Main property which defines the main viewmodel.
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
-            "CA1822:MarkMembersAsStatic",
-            Justification = "This non-static member is needed for data binding purposes.")]
-        public MainViewModel Main
-        {
-            get
-            {
-                return _main;
-            }
-        }
-    }
+		public static FormulaireInfosClubVM FormulaireInfosClubVM {
+			get { return new FormulaireInfosClubVM(); }
+		}
+		#endregion
+	}
 }
