018efe60ed6f80601bdf8d4d90b6b2d95cfd1fdd
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-02-20 16:25:58 +0000

    ajout du VM consultation villes

---
+++ branches/2.0/gestadh45.business/ViewModel/VillesVM/ConsultationVillesVM.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Windows.Data;
+using gestadh45.dal;
+
+namespace gestadh45.business.ViewModel.VillesVM
+{
+	public class ConsultationVillesVM : VMConsultationBase
+	{
+		#region Villes
+		private ICollectionView _villes;
+
+		/// <summary>
+		/// Obtient/Définit la liste des villes
+		/// </summary>
+		public ICollectionView Villes {
+			get { return this._villes; }
+			set {
+				if (this._villes != value) {
+					this._villes = value;
+					this.RaisePropertyChanged(() => this.Villes);
+				}
+			}
+		}
+		#endregion
+
+		#region repositories
+		private Repository<Ville> repoMain;
+		#endregion
+
+		public ConsultationVillesVM() {
+			this.repoMain = new Repository<Ville>(this._context);
+			this.PopulateVilles();
+		}
+
+		private void PopulateVilles() {
+			ICollectionView defaultView = CollectionViewSource.GetDefaultView(this.repoMain.GetAll());
+			defaultView.SortDescriptions.Add(new SortDescription("Libelle", ListSortDirection.Ascending));
+			this.Villes = defaultView;
+		}
+	}
+}
