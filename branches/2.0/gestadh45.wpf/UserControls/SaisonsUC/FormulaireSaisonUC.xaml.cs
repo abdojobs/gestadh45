b1f0ebe3b165c750d42b971e773eeda46bb96da6
guillaume.porte@gmail.com <guillaume.porte@gmail.com@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-02-22 15:46:15 +0000

    travail sur form saison

---
+++ branches/2.0/gestadh45.wpf/UserControls/SaisonsUC/FormulaireSaisonUC.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace gestadh45.wpf.UserControls.Saisons
+{
+	/// <summary>
+	/// Logique d'interaction pour FormulaireSaisonUC.xaml
+	/// </summary>
+	public partial class FormulaireSaisonUC : UserControl
+	{
+		public FormulaireSaisonUC() {
+			InitializeComponent();
+		}
+	}
+}
