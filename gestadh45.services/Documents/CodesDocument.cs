d44747169071776ac03c4c39be1f0e89e91a8f1a
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-03-14 23:00:05 +0000

    Travail sur le service documents

--- gestadh45.services/Documents/CodesDocument.cs
+++ gestadh45.services/Documents/CodesDocument.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace gestadh45.services.Documents
 {
-	public class CodesDocuments
+	public class CodesDocument
 	{
 		public const string FicheInscriptionPDF = "FicheInscriptionPDF";
 		public const string AttestationPDF = "AttestationPDF";
