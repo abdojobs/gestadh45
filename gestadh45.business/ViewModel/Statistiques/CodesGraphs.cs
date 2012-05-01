982b9530697007d466cd090119c00934a23a4635
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-05-01 22:05:41 +0000

    Debut du travail sur les graphs

---
+++ gestadh45.business/ViewModel/Statistiques/CodesGraphs.cs
@@ -0,0 +1,9 @@
+﻿
+namespace gestadh45.business.ViewModel.Statistiques
+{
+	public class CodesGraphs
+	{
+		public const string RemplissageGroupes = "RemplissageGroupes";
+		public const string RepartitionHommesFemmes = "RepartitionHommesFemmes";
+	}
+}
