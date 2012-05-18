f96c64393a3cc435e3db0c1be959744b82beacc4
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-05-18 15:26:16 +0000

    ajout about box

--- gestadh45.wpf/Properties/AssemblyInfo.cs
+++ gestadh45.wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("gestadh45.wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GestAdh45")]
+[assembly: AssemblyDescription("Gestion des adhérents d'un club sportif")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Guillaume Porte")]
-[assembly: AssemblyProduct("gestadh45.wpf")]
+[assembly: AssemblyProduct("GestAdh45")]
 [assembly: AssemblyCopyright("Copyright © Guillaume Porte 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
