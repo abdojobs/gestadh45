e19d869f5bd651dc5a91b2c674c1943aedc7388c
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-05-21 11:52:02 +0000

    ajout de la gestion de la mise au rebut via une date de mise au rebuit

--- gestadh45.dal/Equipement.cs
+++ gestadh45.dal/Equipement.cs
@@ -23,6 +23,7 @@ namespace gestadh45.dal
         public string Commentaire { get; set; }
         public System.Guid ID_Marque { get; set; }
         public System.Guid ID_Categorie { get; set; }
+        public Nullable<System.DateTime> DateMiseAuRebut { get; set; }
     
         public virtual Marque Marque { get; set; }
         public virtual Categorie Categorie { get; set; }
