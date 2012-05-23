b01bc9d2c363a8b2744e0a9f901a6050b21b24f3
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-05-23 20:54:29 +0000

    suppression du vieux trunk

--- trunk/gestadh45.service/Documents/CodesDocument.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace gestadh45.service.Documents
-{
-	public class CodesDocument
-	{
-		public const string FicheInscriptionPDF = "FicheInscriptionPDF";
-		public const string AttestationPDF = "AttestationPDF";
-	}
-}
