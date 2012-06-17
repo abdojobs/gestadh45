e7ba79936d0ded0ca0035febf94e93549a390296
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-06-17 07:56:59 +0000

    Affichage du chemin vers la source de données sur l'interface (#50)

---
+++ trunk/gestadh45.business/PersonalizedMsg/NMShowInfosDataSource.cs
@@ -0,0 +1,9 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+
+namespace gestadh45.business.PersonalizedMsg
+{
+	public class NMShowInfosDataSource : NotificationMessage<string>
+	{
+		public NMShowInfosDataSource(string infosDataSource) : base(infosDataSource, NMType.NMShowInfosDataSource) { }
+	}
+}
