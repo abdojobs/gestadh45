c6ef22ee3e24c26f3e594a5a72150e1110bea42e
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-06-17 18:41:43 +0000

    Ajout de l'écran de répartition des effectifs

---
+++ trunk/gestadh45.wpf/UserControls/RepartitionAdherentsUC/EcranRepartitionAdherentsUC.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace gestadh45.wpf.UserControls.RepartitionAdherentsUC
+{
+	/// <summary>
+	/// Logique d'interaction pour RepartitionAdherentsUC.xaml
+	/// </summary>
+	public partial class EcranRepartitionAdherentsUC : UserControl
+	{
+		public EcranRepartitionAdherentsUC() {
+			InitializeComponent();
+		}
+	}
+}
