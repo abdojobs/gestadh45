374eb349a97e3924d373ad9cb2860cb3eac5467d
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-06-22 14:37:42 +0000

    duree vie epi

---
+++ gestadh45.dal/PersonalizedObj/DureeDeVie.cs
@@ -0,0 +1,10 @@
+﻿
+namespace gestadh45.dal
+{
+	public partial class DureeDeVie
+	{
+		public override string ToString() {
+			return string.Format("{0} ({1}ans, {2} mois)", this.Libelle, this.NbAnnee, this.NbMois);
+		}
+	}
+}
