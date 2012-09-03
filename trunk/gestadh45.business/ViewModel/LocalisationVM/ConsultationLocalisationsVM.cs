33b94bb1d15fce71aebc0b086669390e89113a72
guillaume.porte <guillaume.porte@d611e88d-6bf9-229a-62c8-66e43ab2aeb8>
2012-09-03 20:27:59 +0000

    Création d'un VM de consultation générique et implémentation sur les vm de consultation du référentiel

--- trunk/gestadh45.business/ViewModel/LocalisationVM/ConsultationLocalisationsVM.cs
+++ trunk/gestadh45.business/ViewModel/LocalisationVM/ConsultationLocalisationsVM.cs
@@ -1,91 +1,12 @@
-﻿using System.Linq;
-using GalaSoft.MvvmLight.Messaging;
-using gestadh45.business.PersonalizedMsg;
-using gestadh45.dal;
+﻿using gestadh45.dal;
 
 namespace gestadh45.business.ViewModel.LocalisationVM
 {
-	public class ConsultationLocalisationsVM : VMConsultationBase
+	public class ConsultationLocalisationsVM : GenericConsultationVM<Localisation>
 	{
-		#region Localisations
-		private IOrderedEnumerable<Localisation> _localisations;
-
-		/// <summary>
-		/// Obtient/Définit la liste des localisations
-		/// </summary>
-		public IOrderedEnumerable<Localisation> Localisations {
-			get { return this._localisations; }
-			set {
-				if (this._localisations != value) {
-					this._localisations = value;
-					this.RaisePropertyChanged(() => this.Localisations);
-				}
-			}
-		}
-		#endregion
-
-
-		#region SelectedLocalisation
-		private Localisation _selectedLocalisation;
-
-		/// <summary>
-		/// Obtient/Définit la localisation sélectionnée
-		/// </summary>
-		public Localisation SelectedLocalisation {
-			get { return this._selectedLocalisation; }
-			set {
-				if (this._selectedLocalisation != value) {
-					this._selectedLocalisation = value;
-					this.RaisePropertyChanged(() => this.SelectedLocalisation);
-				}
-			}
-		}
-		#endregion
-				
-
-		#region Repositories
-		private Repository<Localisation> _repoMain;
-		#endregion
-
-		#region Constructeur
-		public ConsultationLocalisationsVM() {
-			this._repoMain = new Repository<Localisation>(this._context);
-			this.PopulateLocalisations();
-		}
-		#endregion
-
-		private void PopulateLocalisations() {
-			this.Localisations = this._repoMain.GetAll().OrderBy(l => l.Libelle);
-		}
-
-		#region ShowDetailsCommand
-		public override void ExecuteShowDetailsCommand(object selectedItem) {
-			if (selectedItem is Localisation) {
-				this.SelectedLocalisation = selectedItem as Localisation;
-			}
-		}
-		#endregion
-
-		#region DeleteCommand
-		public override bool CanExecuteDeleteCommand() {
-			return this.SelectedLocalisation != null && this.SelectedLocalisation.Equipements.Count == 0;
-		}
-
-		public override void ExecuteDeleteCommand() {
-			if (this.SelectedLocalisation != null) {
-				this._repoMain.Delete(this.SelectedLocalisation);
-				this._repoMain.Save();
-				this.PopulateLocalisations();
-				this.SelectedLocalisation = this.Localisations.FirstOrDefault();
-
-				this.ShowUserNotification(ResLocalisation.InfoSuppressionLocalisation);
-			}
-		}
-		#endregion
-
 		#region CreateCommand
 		public override void ExecuteCreateCommand() {
-			Messenger.Default.Send<NMShowUC>(new NMShowUC(CodesUC.FormulaireLocalisation));
+			this.ShowUC(CodesUC.FormulaireLocalisation);
 		}
 		#endregion
 	}
--- trunk/gestadh45.business/ViewModel/TranchesAgeVM/ConsultationTranchesAgeVM.cs
+++ trunk/gestadh45.business/ViewModel/TranchesAgeVM/ConsultationTranchesAgeVM.cs
@@ -1,84 +1,9 @@
-﻿using System.Linq;
-using GalaSoft.MvvmLight.Messaging;
-using gestadh45.business.PersonalizedMsg;
-using gestadh45.dal;
+﻿using gestadh45.dal;
 
 namespace gestadh45.business.ViewModel.TranchesAgeVM
 {
-	public class ConsultationTranchesAgeVM : VMConsultationBase
+	public class ConsultationTranchesAgeVM : GenericConsultationVM<TrancheAge>
 	{
-		#region TrancheAges
-		private IOrderedEnumerable<TrancheAge> _tranchesAge;
-
-		/// <summary>
-		/// Obtient/Définit la liste des tranches d'âge
-		/// </summary>
-		public IOrderedEnumerable<TrancheAge> TranchesAge {
-			get { return this._tranchesAge; }
-			set {
-				if (this._tranchesAge != value) {
-					this._tranchesAge = value;
-					this.RaisePropertyChanged(() => this.TranchesAge);
-				}
-			}
-		}
-		#endregion
-
-		#region SelectedTrancheAge
-		private TrancheAge _selectedTrancheAge;
-
-		/// <summary>
-		/// Obtient/Définit la tranche d'âge sélectionnée
-		/// </summary>
-		public TrancheAge SelectedTrancheAge {
-			get { return this._selectedTrancheAge; }
-			set {
-				if (this._selectedTrancheAge != value) {
-					this._selectedTrancheAge = value;
-					this.RaisePropertyChanged(() => this.SelectedTrancheAge);
-				}
-			}
-		}
-		#endregion
-
-		#region repositories
-		private Repository<TrancheAge> _repoMain;
-		#endregion
-
-		public ConsultationTranchesAgeVM() {
-			this._repoMain = new Repository<TrancheAge>(this._context);
-			this.PopulateTranchesAge();
-		}
-
-		private void PopulateTranchesAge() {
-			this.TranchesAge = this._repoMain.GetAll().OrderBy(t => t.AgeInf);
-		}
-
-		#region ShowDetailsCommand
-		public override void ExecuteShowDetailsCommand(object selectedItem) {
-			if (selectedItem is TrancheAge) {
-				this.SelectedTrancheAge = selectedItem as TrancheAge;
-			}
-		}
-		#endregion
-
-		#region DeleteCommand
-		public override bool CanExecuteDeleteCommand() {
-			return this.SelectedTrancheAge != null;
-		}
-
-		public override void ExecuteDeleteCommand() {
-			if (this.SelectedTrancheAge != null) {
-				this._repoMain.Delete(this.SelectedTrancheAge);
-				this._repoMain.Save();
-				this.PopulateTranchesAge();
-				this.SelectedTrancheAge = this.TranchesAge.FirstOrDefault();
-
-				this.ShowUserNotification(ResTranchesAge.InfoTrancheAgeSupprimee);
-			}
-		}
-		#endregion
-
 		#region CreateCommand
 		public override void ExecuteCreateCommand() {
 			this.ShowUC(CodesUC.FormulaireTrancheAge);
